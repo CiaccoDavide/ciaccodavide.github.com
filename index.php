984a4cc6a0d7d61bd1182e956f90ebf7f28f8cf3
CiaccoDavide <ciaccodavide@gmail.com>
2013-11-28 15:52:43 +0100

    uuuuupas

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Ciacco Davide</title>  
+		<link rel="stylesheet" type="text/css" media="screen" href="./css/php.css" />
+		<link href='http://fonts.googleapis.com/css?family=Lato:100,300,100italic,300italic|Quicksand:300,400|Source+Code+Pro:200,300,400|Raleway:100,200' rel='stylesheet' type='text/css'>
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	</head>
+	<body>
+			<a href="#">
+			<div class="post" id="red">
+				<t>.</t><h2>Titolo</h2>
+				<div class="desc"> Qui ci sarà una breve descrizione...</div>
+			</div>
+			</a>
+			
+			<a href="#">
+			<div class="post" id="green">
+				<t>.</t><h2>Titolo</h2>
+				<div class="desc"> Qui ci sarà una breve descrizione...</div>
+			</div>
+			</a>
+			
+			<a href="#">
+			<div class="post" id="blue">
+				<t>.</t><h2>Titolo</h2>
+				<div class="desc"> Qui ci sarà una breve descrizione...</div>
+			</div>
+			</a>
+	</body>
+</html>
---
+++ relax/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Ciacco Davide</title>  
+		<link rel="stylesheet" type="text/css" media="screen" href="./css/relaxphp.css" />
+		<link href='http://fonts.googleapis.com/css?family=Lato:100,300,100italic,300italic|Quicksand:300,400|Source+Code+Pro:200,300,400|Raleway:100,200' rel='stylesheet' type='text/css'>
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	</head>
+	<body>
+			<a href="#">
+			<div class="post" id="red">
+				<t>.</t><h2>Video</h2>
+				<div class="desc"> Qui ci sarà il video...</div>
+			</div>
+			</a>
+			
+			<a href="#">
+			<div class="post" id="green">
+				<t>.</t><h2>GIF</h2>
+				<div class="desc"> Qui ci sarà una gif...</div>
+			</div>
+			</a>
+			
+			<a href="#">
+			<div class="post" id="blue">
+				<t>.</t><h2>Immagine</h2>
+				<div class="desc"> Qui ci sarà una immagine...</div>
+			</div>
+			</a>
+			
+			<a href="#">
+			<div class="post" id="blue">
+				<t>.</t><h2>Canzone</h2>
+				<div class="desc"> Qui ci sarà il video della canzone...</div>
+			</div>
+			</a>
+	</body>
+</html>
