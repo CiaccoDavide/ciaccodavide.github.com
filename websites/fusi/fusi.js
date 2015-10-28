9eb977738538f6bdb7ca7447bbdf12b5ea336464
CiaccoDavide <davideciacco@CDMB.local>
2015-10-28 21:35:19 +0100

    fusi

--- websites/fusi/fusi.js
+++ websites/fusi/fusi.js
@@ -1,3 +1,15 @@
+// istruzioni:
+// per spostare l'orario di tutti gli orologi
+// modifica la variabile "regolaOrario"
+// ora ho spostato gli orari indietro di un'ora
+// infatti la variabile "regolaOrario" è uguale a "-1"
+// per portare avanti gli orologi di un'ora basta aggiungere 1 a quella variabile
+// quindi per aggiungere 1 a "regolaOrario" sostituisci il "-1" con 0:
+//   al posto di "var regolaOrario = -1;"
+// dovrà esserci "var regolaOrario = 0; "
+
+var regolaOrario = -1;
+
 var c = document.getElementById("canvas");
 var ctx = c.getContext("2d");
 
@@ -27,9 +39,20 @@ function draw() {
     ctx.fillStyle = '#7f7f7f';
     ctx.font = "8px Verdana";
     ctx.fillText("MILANO",17,71);
+    //posizione0 MILANO +2
+    drawLancette(0,2);
+
     ctx.fillText("LONDON",87,71);
+    //posizione0 LONDON +1
+    drawLancette(1,1);
+
     ctx.fillText("TOKYO",162,71);
+    //posizione0 TOKYO +9
+    drawLancette(2,9);
+
     ctx.fillText("NEW YORK",225,71);
+    //posizione0 NEW YORK -4 (+20)
+    drawLancette(3,20);
 
     if(s<59){
         s++;
@@ -46,11 +69,32 @@ function draw() {
         }
     }
     //lancette
-    drawLancette();
+    //drawLancette();
 }
+function drawLancette(pos,f) {
+    fuso=f+regolaOrario;
+    posx=34.5+(pos*71);
+    posy=34.5;
+    ctx.beginPath();
+    ctx.strokeStyle = '#444';
+    angolo=toRadians(s*6-90);
+    ctx.moveTo(posx+(4*Math.cos(angolo)),posy+(4*Math.sin(angolo)));
+    ctx.lineTo(posx+(21+4)*Math.cos(angolo),posy+(21+4)*Math.sin(angolo));
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.strokeStyle = '#009ee0';
+    angolo=toRadians(m*6-90);
+    ctx.moveTo(posx+4*Math.cos(angolo),posy+4*Math.sin(angolo));
+    ctx.lineTo(posx+(18+4)*Math.cos(angolo),posy+(18+4)*Math.sin(angolo));
+    ctx.stroke();
+    angolo=toRadians((360/12)*(h+fuso)-90);
+    ctx.moveTo(posx+4*Math.cos(angolo),posy+4*Math.sin(angolo));
+    ctx.lineTo(posx+(12+4)*Math.cos(angolo),posy+(12+4)*Math.sin(angolo));
+    ctx.stroke();
+}/*
 function drawLancette() {
     //posizione0 MILANO +2
-    fuso=2;
+    fuso=2+regolaOrario;
     posx=34.5;
     posy=34.5;
     ctx.beginPath();
@@ -70,7 +114,7 @@ function drawLancette() {
     ctx.lineTo(posx+(12+4)*Math.cos(angolo),posy+(12+4)*Math.sin(angolo));
     ctx.stroke();
     //posizione0 LONDON +1
-    fuso=1;
+    fuso=1+regolaOrario;
     posx=105.5;
     posy=34.5;
     ctx.beginPath();
@@ -90,7 +134,7 @@ function drawLancette() {
     ctx.lineTo(posx+(12+4)*Math.cos(angolo),posy+(12+4)*Math.sin(angolo));
     ctx.stroke();
     //posizione0 TOKYO +9
-    fuso=9;
+    fuso=9+regolaOrario;
     posx=176.5;
     posy=34.5;
     ctx.beginPath();
@@ -110,7 +154,7 @@ function drawLancette() {
     ctx.lineTo(posx+(12+4)*Math.cos(angolo),posy+(12+4)*Math.sin(angolo));
     ctx.stroke();
     //posizione0 NEW YORK -4 (+20)
-    fuso=20;
+    fuso=20+regolaOrario;
     posx=247.5;
     posy=34.5;
     ctx.beginPath();
@@ -129,7 +173,7 @@ function drawLancette() {
     ctx.moveTo(posx+4*Math.cos(angolo),posy+4*Math.sin(angolo));
     ctx.lineTo(posx+(12+4)*Math.cos(angolo),posy+(12+4)*Math.sin(angolo));
     ctx.stroke();
-}
+}*/
 function toRadians (angle) {
   return angle * (Math.PI / 180);
 }
